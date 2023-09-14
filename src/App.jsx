72a896322d15014a8fd51a9e92129c8f21aaabc0
Your Name <you@example.com>
2023-09-15 02:31:24 +0600

    nav section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './components/Home/home'
+function App() {
+  
+
+  return (
+    <>
+   <Home></Home>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h3>fdsfd</h3>
+            <h3>fdsfd</h3>
+            <h1 className='mt-5 text-center font-bold text-3xl'>Course Registration</h1>
+        </div>
+    );
+};
+export default Home;
