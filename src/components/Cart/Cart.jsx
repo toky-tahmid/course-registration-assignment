2ed43fb04880e0d4b212bee66043bd7bc0c133ee
Your Name <you@example.com>
2023-09-15 20:17:46 +0600

    carts total and credit total added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,32 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-const Cart = ({ selectedCards,remaining,totalCost }) => {
+const Cart = ({ selectedCards,remaining,totalCost,total }) => {
   return (
     <>
-    <div className="border-2px">
-        <h2>Credit Hour Remaining {remaining} hr</h2>
+    <div className="bg-slate-100 p-4 m-4 rounded-xl">
+    <div>
+        <h2 className="text-[#2F80ED] font-bold">Credit Hour Remaining {remaining} hr</h2>
     </div>
+    <hr />
       <div>
-        <h2>Course Name: </h2>
-          {selectedCards.map((card) => (
-            <li key={card.id}>{card.title}</li>
-          ))}
+        <h2 className="font-bold text-2xl">Course Name: </h2>
+        <div>
+  <h2>Selected Courses:</h2>
+  <ol start={1}>
+    {selectedCards.map((card,index) => (
+     <li key={card.id}>{`${index + 1}. ${card.title}`}</li>
+    ))}
+  </ol>
+</div>
       </div>
       <div>
-        <h4>Total Credit Hour : {totalCost}</h4>
+        <h4 className="font-semibold">Total Credit Hour : {totalCost}</h4>
       </div>
+      <hr />
+      <div>
+        <h4 className="font-semibold">Total Price: {total}</h4>
+      </div>
+    </div>
     </>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,16 +8,19 @@ const Home = () => {
   const [selectedCards, setSelectedCards] = useState([]);
   const [remaining,setRemaining] = useState(0);
   const [totalCost,setTotalCost] = useState(0);
+  const [total,setTotal] = useState(0);
   useEffect(() => {
     fetch("./data.json")
       .then((response) => response.json())
       .then((data) => setCards(data));
   }, []);
-
-
 const handleSelectCard = (card)=> {
     const isExist = selectedCards.find((item) => item.id === card.id)
     let count = card.Credit;
+    let total = card.price;
+    selectedCards.forEach((item) => {
+        total += item.price;
+    })
 if(isExist) {
     alert("Please")}
 else{
@@ -29,6 +32,7 @@ if(count > 20){
     alert("help")
 }
 else{
+    setTotal(total)
     setTotalCost(count)
     setRemaining(totalRemaining)
         setSelectedCards([...selectedCards, card]);
@@ -40,7 +44,7 @@ else{
           Course Registration
         </h1>
       </div>
-      <div className="container flex justify-between">
+      <div className="container flex justify-evenly">
         <div className="home-container">
             <div className="card-container ml-10 mt-4 grid grid-cols-3 gap-6">
            {
@@ -56,23 +60,20 @@ else{
                   </p>
                 </div>
                 <div className="info flex justify-between">
-                  <p className="font-bold"> $Price:{card.price}</p>
-                  <p className="font-bold">Credit:{card.Credit}</p>
+                  <p className="font-bold ml-3"> $Price:{card.price}</p>
+                  <p className="font-bold mr-3">Credit:{card.Credit}</p>
                 </div>
                 <div>
-                  <button onClick={()=> handleSelectCard(card)}  className="btn btn-warning text-white w-40 ml-8 mt-3">
+                  <button onClick={()=> handleSelectCard(card)}  className="btn bg-[#2F80ED] text-white w-40  ml-8 mt-4">
                     Select
                   </button>
                 </div>
               </div>
-            ))
-
-           }
+            ))}
             </div>
-        
         </div>
-        <div className="cart ">
-          <Cart selectedCards={selectedCards} remaining={remaining} totalCost={totalCost} ></Cart>
+        <div className="cart mt-3">
+          <Cart selectedCards={selectedCards} remaining={remaining} totalCost={totalCost} total={total} ></Cart>
         </div>
       </div>
     </>
